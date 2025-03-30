f5de9ce464ac78adfec79a6de82767000f1e2999
Aldo Callisaya <acallisaya@frba.utn.edu.ar>
2025-03-30 16:04:59 -0300

    primer commit

---
+++ src/main/java/com/example/apifixture/ApiFixtureApplication.java
@@ -0,0 +1,13 @@
+package com.example.apifixture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiFixtureApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ApiFixtureApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/apifixture/ApiFixtureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apifixture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiFixtureApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
